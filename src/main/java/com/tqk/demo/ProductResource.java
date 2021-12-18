d7744929437bfb3a588fd4be6978b9de90693080
quangkhai88 <canhcung88@gmail.com>
2021-12-18 15:08:55 +0100

    update

--- src/main/java/com/tqk/demo/ProductResource.java
+++ src/main/java/com/tqk/demo/ProductResource.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +19,11 @@ public class ProductResource {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getProductPage() {
-
-        SecureRandom random = new SecureRandom(); // Compliant for security-sensitive use cases
-        List<Product> list = new ArrayList<>();
-        int i = 0;
-        while (i < random.nextInt(30)) {
-            list.add(new Product("Project name" + i, i));
-            i++;
-        }
-        return list;
+        return createProducts();
     }
 
     public List<Product> createProducts() {
-        return generator.objects(Product.class, 5).collect(Collectors.toList());
+        SecureRandom random = new SecureRandom(); 
+        return generator.objects(Product.class, random.nextInt(30)).collect(Collectors.toList());
     }
 }
